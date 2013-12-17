c512c494c5a5b7051cfd9f4c538d8a5febe553a4
priscrisant <pr.risant@gmail.com>
2013-12-17 20:48:21 -0300

    modified code

--- pass.py
+++ pass.py
@@ -9,20 +9,26 @@ class Psw:
         print "------------------------------"
 
     def Info(self):
-        user = raw_input ("Please, enter the main password to have access:\n")
+        user = raw_input ("Please, enter the main password in lower case to have access:\n")
         if user == "mainpassword":
             print "Access Guaranteed! Welcome to your Password Manager!"
         else:
             print "Something went wrong! You typed an invalid information. Exiting the program!"
-
+            sys.exit(1)
 
     def main(self):
-        user_2 = raw_input ("Please type the name of the website/software you want to know the password:\n")
+        user_2 = raw_input ("Please type the name of the website/software you want to know the password in lower case:\n")
         if user_2 == "outlook":
-            print "Your Outlook Password is: HAYHEYHSH"
+            print "Your Outlook Password is: ********"
+        elif user_2 == "yahoo":
+            print "Your Yahoo Password is: *********"
+        elif user_2 == "instagram":
+            print "Your instagram Password is: **********"
+        elif user_2 == "gmail":
+            print "Your Gmail Password is: **********"
         else:
             print "There's no such information here."
-
+            sys.exit(2)
 
 if __name__ == "__main__":
     root = Psw()
